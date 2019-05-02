228c8c11d0eb1e5358f9e9029eb1f9587ce4ff92
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2019-05-02 15:48:32 +0300

    Minor Update

--- app/src/main/java/ceid/katefidis/smartit/AddDeviceFragment.java
+++ app/src/main/java/ceid/katefidis/smartit/AddDeviceFragment.java
@@ -16,6 +16,8 @@ public class AddDeviceFragment extends Fragment {
         // Required empty public constructor
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
