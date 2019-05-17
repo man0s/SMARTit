5f4320638e0d5e5b3ee5c4fe3489b362437de91a
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2019-05-17 14:59:19 +0300

    ADD Update Implemented

--- app/src/main/java/ceid/katefidis/smartit/SettingsFragment.java
+++ app/src/main/java/ceid/katefidis/smartit/SettingsFragment.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class SettingsFragment extends Fragment {
 
@@ -19,6 +21,14 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        LinearLayout app_layer = (LinearLayout) view.findViewById (R.id.updates);
+        app_layer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "You have already the latest version!", Toast.LENGTH_LONG).show();
+            }
+        });
 
         return view;
     }
